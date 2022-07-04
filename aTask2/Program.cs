1ddef8ad511c4c4674165eb01c4b10b6da72f0d5
Fray <kozar20000@mail.ru>
2022-07-04 20:55:31 +0300

    task2

---
+++ aTask2/Program.cs
@@ -0,0 +1,18 @@
+﻿int num (int N)
+{
+    int result = 0;
+    int div = 1;
+    while(N / div >= 1)
+    {
+        div *= 10;
+        result += 1;
+    }
+
+    Console.WriteLine(result);
+    return result;
+}
+
+num(5);
+num(17);
+num(175);
+num(87579);
