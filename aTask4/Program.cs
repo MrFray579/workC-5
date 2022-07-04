6eee2d93091c7b8828b8a0c1597f868258d3c45b
Fray <kozar20000@mail.ru>
2022-07-04 21:31:14 +0300

    task4

---
+++ aTask4/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] arr(int N)
+{
+    int[] result = new int[N];
+    for(int i = 0; i <= 8; i++)
+    {
+        int a = new Random().Next(0, 2);
+        result[i] = a;
+        Console.Write(result[i] + " ");
+         
+    }
+    return result;
+
+}
+
+arr(15);
