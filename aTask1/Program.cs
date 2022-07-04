680ceb04af05ccd7da88d293cf7689420ff1c472
Fray <kozar20000@mail.ru>
2022-07-04 20:38:12 +0300

    task1

---
+++ aTask1/Program.cs
@@ -0,0 +1,13 @@
+﻿int calculed (int A)
+{
+    int[] arr = new int[A];
+    int result  = 0;
+    for (int i = 0; i <= A; i++)
+    {
+        result += i; 
+    }
+    Console.WriteLine("calculed from 1 to {0} is {1}", A, result);
+    return result;
+}
+
+calculed(7);
