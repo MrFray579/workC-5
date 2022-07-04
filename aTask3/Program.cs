bdd3a5404e2585320d17cfd29a967b1a66eb4b9a
Fray <kozar20000@mail.ru>
2022-07-04 21:05:07 +0300

    task3

---
+++ aTask3/Program.cs
@@ -0,0 +1,13 @@
+﻿int sum (int N)
+{
+    int result =1;
+    for(int i = 1; i <= N; i++)
+    {
+        result *= i;
+    }
+    Console.WriteLine(result);
+    return result;
+}
+
+sum(10);
+sum(4);
